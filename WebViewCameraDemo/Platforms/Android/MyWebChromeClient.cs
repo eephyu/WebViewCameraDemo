572b9aa1ab5648e260656d0f9033354f89f17c34
eephyu <32757774+eephyu@users.noreply.github.com>
2024-09-18 14:48:05 +0800

    Update MyWebChromeClient.cs
    Adjust source code to have camera option by using OnShowFileChooser

--- WebViewCameraDemo/Platforms/Android/MyWebChromeClient.cs
+++ WebViewCameraDemo/Platforms/Android/MyWebChromeClient.cs
@@ -1,4 +1,6 @@
-﻿using Android.Webkit;
+using Android.OS;
+using Android.Provider;
+using Android.Webkit;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Platform;
 using System;
@@ -7,6 +9,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Android.Content;
+using Android.Icu.Text;
+using Android.Icu.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
 namespace WebViewCameraDemo.Platforms.Android
 {
     internal class MyWebChromeClient : MauiWebChromeClient
@@ -14,6 +25,32 @@ namespace WebViewCameraDemo.Platforms.Android
         public MyWebChromeClient(IWebViewHandler handler) : base(handler)
         {
 
+        }
+
+        public static IValueCallback mUploadCallbackAboveL;
+        public override bool OnShowFileChooser(global::Android.Webkit.WebView webView, IValueCallback filePathCallback, FileChooserParams fileChooserParams)
+        {
+            mUploadCallbackAboveL = filePathCallback;
+            takePhoto();
+            return true;
+        }
+
+        private void takePhoto()
+        {
+            //close the policy, if not you will get Android.OS.FileUriExposedException:exposed beyond app through ClipData.Item.getUri()'
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.SetVmPolicy(builder.Build());
+
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            intent.PutExtra(MediaStore.ExtraOutput, MainActivity.imageUri);
+            MainActivity.Instance.StartActivityForResult(intent, MainActivity.REQUEST_CODE);
+
+
+            // Select the picture from the local file
+            //        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            //        i.addCategory(Intent.CATEGORY_OPENABLE);
+            //        i.setType("image/*");
+            //        startActivityForResult(Intent.createChooser(i, "Image Chooser"), REQUEST_CODE);
         }
 
         public override void OnPermissionRequest(PermissionRequest request)
@@ -25,14 +62,23 @@ namespace WebViewCameraDemo.Platforms.Android
                 if (resource.Equals(PermissionRequest.ResourceVideoCapture, StringComparison.OrdinalIgnoreCase))
                 {
                     // Get the status of the .NET MAUI app's access to the camera
-                    PermissionStatus status = Permissions.CheckStatusAsync<Permissions.Camera>().Result;
+                    //PermissionStatus status = Permissions.CheckStatusAsync<Permissions.Camera>().Result;
 
-                    // Deny the web page's request if the app's access to the camera is not "Granted"
-                    if (status != PermissionStatus.Granted)
-                        request.Deny();
-                    else
+                    //// Deny the web page's request if the app's access to the camera is not "Granted"
+                    //if (status != PermissionStatus.Granted)
+                    //    request.Deny();
+                    //else
+                    //    request.Grant(request.GetResources());
+
+                    try
+                    {
                         request.Grant(request.GetResources());
-
+                        base.OnPermissionRequest(request);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
                     return;
                 }
             }
