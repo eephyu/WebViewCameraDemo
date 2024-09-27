17381e4ed1f01643822dcc32a50253ddc277d55f
eephyu <32757774+eephyu@users.noreply.github.com>
2024-09-27 14:21:02 +0800

    Update MyWebChromeClient.cs
    Added 3 buttons which is for Camera, Photo and Cancel

--- WebViewCameraDemo/Platforms/Android/MyWebChromeClient.cs
+++ WebViewCameraDemo/Platforms/Android/MyWebChromeClient.cs
@@ -16,7 +16,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Android.Annotation;
+using Android.App;
+using AndroidNet = Android.Net;
+using Android.Graphics.Drawables;
+using AndroidGraphics = Android.Graphics;
+using Android.Views;
+using AndroidWidget = Android.Widget;
+using Android.Widget;
 
 namespace WebViewCameraDemo.Platforms.Android
 {
@@ -28,11 +35,92 @@ namespace WebViewCameraDemo.Platforms.Android
         }
 
         public static IValueCallback mUploadCallbackAboveL;
+        string _photoPath;
         public override bool OnShowFileChooser(global::Android.Webkit.WebView webView, IValueCallback filePathCallback, FileChooserParams fileChooserParams)
         {
-            mUploadCallbackAboveL = filePathCallback;
-            takePhoto();
+            #region Method 1
+            var context = Platform.CurrentActivity;
+
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.Instance); //, Resource.Style.CustomAlertDialogTheme //, global::WebViewCameraDemo.Resource.Style.CustomAlertDialogTheme
+            alertDialog.SetTitle("Take picture or choose photo.");
+            
+            alertDialog.SetNeutralButton("Camera", async (sender, alertArgs) =>
+            {
+                try
+                {
+                    var photo = await MediaPicker.CapturePhotoAsync();
+                    var uri = await LoadPhotoAsync(photo);
+                    filePathCallback.OnReceiveValue(uri);
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+                }
+            });
+
+            alertDialog.SetNegativeButton("Photo", async (sender, alertArgs) =>
+            {
+                try
+                {
+                    var photo = await MediaPicker.PickPhotoAsync();
+                    var uri = await LoadPhotoAsync(photo);
+                    filePathCallback.OnReceiveValue(uri);
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine($"PickPhotoAsync THREW: {ex.Message}");
+                }
+            });
+            alertDialog.SetPositiveButton("Cancel", (sender, alertArgs) =>
+            {
+                filePathCallback.OnReceiveValue(null);
+            });
+
+            var dialog = alertDialog.Create();
+            dialog.Show();
+
+            #region button icons
+            // Access and style the buttons
+            var camButton = dialog.GetButton((int)DialogButtonType.Neutral);            
+            camButton.SetBackgroundColor(AndroidGraphics.Color.Transparent); // Set custom background color
+            camButton.SetTextColor(AndroidGraphics.Color.DarkGray); // Set custom text color
+            Drawable iconCam = context.GetDrawable(Microsoft.Maui.Resource.Drawable.camera);
+            iconCam.SetBounds(0, 0, iconCam.IntrinsicWidth, iconCam.IntrinsicHeight);
+            camButton.SetCompoundDrawables(iconCam, null, null, null);
+
+            var photoButton = dialog.GetButton((int)DialogButtonType.Negative);
+            photoButton.SetBackgroundColor(AndroidGraphics.Color.Transparent); // Set custom background color
+            photoButton.SetTextColor(AndroidGraphics.Color.DarkGray); // Set custom text color
+            Drawable iconPhoto = context.GetDrawable(Microsoft.Maui.Resource.Drawable.gallery);
+            iconPhoto.SetBounds(0, 0, iconCam.IntrinsicWidth, iconCam.IntrinsicHeight);
+            photoButton.SetCompoundDrawables(iconPhoto, null, null, null);
+
+            var cancelButton = dialog.GetButton((int)DialogButtonType.Positive);
+            cancelButton.SetBackgroundColor(AndroidGraphics.Color.Transparent); // Set custom background color
+            cancelButton.SetTextColor(AndroidGraphics.Color.DarkGray); // Set custom text color
+
+            #endregion
+
+            #endregion
+
             return true;
+        }
+        async Task<AndroidNet.Uri[]> LoadPhotoAsync(FileResult photo)
+        {
+            // cancelled
+            if (photo == null)
+            {
+                _photoPath = null;
+                return null;
+            }
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = System.IO.File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+            _photoPath = newFile;
+            AndroidNet.Uri uri = AndroidNet.Uri.FromFile(new Java.IO.File(_photoPath));
+            return new AndroidNet.Uri[] { uri };
         }
 
         private void takePhoto()
@@ -44,7 +132,6 @@ namespace WebViewCameraDemo.Platforms.Android
             Intent intent = new Intent(MediaStore.ActionImageCapture);
             intent.PutExtra(MediaStore.ExtraOutput, MainActivity.imageUri);
             MainActivity.Instance.StartActivityForResult(intent, MainActivity.REQUEST_CODE);
-
 
             // Select the picture from the local file
             //        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
@@ -85,5 +172,7 @@ namespace WebViewCameraDemo.Platforms.Android
 
             base.OnPermissionRequest(request);
         }
+
+
     }
 }
